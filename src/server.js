01f14e032072dbec388ebba0adfcb7af888fbf87
chidii03 <stephenokwu795@gmail.com>
2026-01-29 02:22:59 +0100

    Update server.js
    app update

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import './db/init.js';
 
 const PORT = process.env.PORT || 5000; 
 
-server.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Steve O Bizz Store Backend running on port ${PORT}`);
 });
